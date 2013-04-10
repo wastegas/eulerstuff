864190acc30184be1e3160a7715d98b581613420
wastegas <wastegas@gmail.com>
2013-04-09 22:35:02 -1000

    prob 13

---
+++ euler13.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <string>
+#include <gmp.h>
+
+using namespace std;
+
+class euler13
+{
+public:
+	euler13(){sumDigits();}
+	~euler13(){}
+	void sumDigits();
+private:
+	mpz_t sum;
+	mpz_t dline;
+};
+
+void euler13::sumDigits()
+{
+	mpz_init(sum);
+	mpz_init(dline);
+	mpz_set_ui(sum,0);
+	mpz_set_ui(dline,0);
+	ifstream iStream("digits");
+	string line;
+	if(iStream.is_open())
+	{
+		while(getline(iStream,line))
+		{
+			mpz_set_str(dline, line.c_str(), 10);
+			mpz_add(sum,sum, dline);
+		}
+		iStream.close();
+		gmp_printf("%Zd", sum);
+		mpz_clear(dline);
+		mpz_clear(sum);
+	}
+	else
+	{
+		std::cout << "unable to open file" << std::endl;
+	}
+}
+
+int main()
+{
+	euler13 D;
+	cout << endl;
+
+	return 0;
+}
