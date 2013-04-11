6f7dd85e6bf3e43c43c9234c2623195e97dad7b6
wastegas <wastegas@gmail.com>
2013-04-10 22:22:30 -1000

    did minor  changes

--- euler12.cpp
+++ euler12.cpp
@@ -5,45 +5,41 @@
 class TriangleNo
 {
 public:
-	TriangleNo() {getTerm();};
-	void getTerm();
-	void TriangleTerm();
+	TriangleNo()
+	       : Tn(1), n(1), count(0)	
+	{
+		TriangleTerm();
+		std::cout << "Tn " << Tn << " has " << count << " divisors"<< std::endl;
+	}
+
+			
 private:
+	void TriangleTerm()
+	{
+		for(;;)
+		{
+			Tn=(n*(n+1))/2;
+			n++;
+			for(long long d=1; d<=sqrt(Tn); d++)
+			{	
+				if(Tn % d == 0)
+				{
+					count++;
+				}
+			}
+			count *= 2;
+			if(count > 500)
+				break;
+			count = 0;
+		}	
+	
+	}
+
 	long long Tn;
 	long long n;
 	int count;
 };
 
-void TriangleNo::getTerm()
-{
-	Tn=1;
-	n=1;
-	TriangleTerm();
-	std::cout << "Tn " << Tn << " has " << count << " divisors"<< std::endl;
-}
-
-void TriangleNo::TriangleTerm()
-{
-	int p;
-	count = 0;
-	for(;;)
-	{
-		Tn=(n*(n+1))/2;
-		n++;
-		for(long long d=1; d<=sqrt(Tn); d++)
-		{
-			if(Tn % d == 0)
-			{
-				count++;
-			}
-		}
-		count *= 2;
-		if(count > 500)
-			break;
-		count = 0;
-	}
-	
-}
 
 int main()
 {
