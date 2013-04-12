2347239928213c820ed9e2cfe0e1ed2e2402c242
wastegas <wastegas@gmail.com>
2013-04-11 20:46:17 -1000

    prob 20

---
+++ euler20.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <gmp.h>
+#include <cstring>
+
+class factorial
+{
+public:
+	factorial(unsigned int ii) 
+		: i(ii) 
+	{
+		factsum();
+	}
+
+private:
+	void factsum()
+	{	
+	
+		mpz_init(f);
+		mpz_set_ui(f,1);
+	
+		for (unsigned int n = i ; n!=0; n--)
+		{
+			mpz_mul_ui(f, f, n);
+		}
+		strf = mpz_get_str(NULL,10,f);
+		for(int i = 0; i< std::strlen(strf); i++)
+		{
+			fsum += strf[i]-'0';
+		}
+	
+		gmp_printf("%d! %Zd\n",i,f);
+		printf("Sum of digits: %d\n", fsum);
+	}
+
+	unsigned int i;
+	mpz_t f;
+	int fsum=0;
+	char* strf;
+};
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "Usage: euler20 <some number>" << std::endl;
+	}
+
+	factorial F(atoi(argv[1]));
+	
+	return 0;
+}
