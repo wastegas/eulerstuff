3740a435fd7fee2386ef5d1ef91c07dbe72baaad
wastegas <wastegas@gmail.com>
2013-04-13 12:15:19 -1000

    prob 1

---
+++ euler1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int sum = 0;
+	for (int i = 1; i < 1000; ++i)
+		if((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+
+	std::cout << sum << std::endl;
+
+	sum = 0;
+
+
+
+	return 0;
+}
