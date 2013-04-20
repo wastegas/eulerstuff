893b810ce6d1752ea61ecf786d7defb3a578f1a5
wastegas <wastegas@gmail.com>
2013-04-20 11:47:13 -1000

    work in progress

---
+++ euler22.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <cctype>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::map <char, int> alpha;
+	char a = 'A';
+	for(int i=1; i <= 26; i++)
+	{
+		alpha.insert(std::make_pair<char&, int&>(a, i));
+		a++;
+	}
+
+	for(auto it=alpha.begin(); it != alpha.end(); ++it)
+		std::cout << (*it).first << " " << (*it).second << std::endl;
+
+	std::ifstream fs("names.txt");
+	std::string line;
+
+	const std::string delims("\",");
+	std::vector<std::string> nameslist;
+	if(fs.is_open())
+	{
+		while(getline(fs, line))
+		{
+			std::string::size_type begIdx, endIdx;
+			std::string name;	
+			begIdx = line.find_first_not_of(delims);
+			while(begIdx != std::string::npos)
+			{
+				endIdx = line.find_first_of(delims, begIdx);
+				if(endIdx == std::string::npos)
+				{       	
+					endIdx = line.length();
+				}
+				for (int i = begIdx; i <= endIdx-1; ++i)
+				{
+					name += line[i];
+				}
+				nameslist.push_back(name);
+				name.clear();
+				begIdx = line.find_first_not_of(delims, endIdx);
+			}
+		}
+	
+	}	
+	fs.close();
+
+	sort(nameslist.begin(), nameslist.end());
+
+	for(auto it = nameslist.begin(); it != nameslist.end(); ++it)
+		std::cout << (*it) << std::endl;
+	
+	
+
+	return 0;
+
+
+}
