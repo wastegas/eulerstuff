b9755021c42097e5f77a0d1aba271fc7de9654ca
wastegas <wastegas@gmail.com>
2013-04-20 15:54:25 -1000

    work in progress

--- euler21.cpp
+++ euler21.cpp
@@ -5,7 +5,10 @@ void propDiv(int &n, int &pdsum)
 	for(int i = 1; i < n; ++i)
 	{
 		if( n % i == 0)
+		{
+			std::cout << i << ' ';	
 			pdsum += i;
+		}
 	}
 }
 
@@ -14,18 +17,27 @@ int main()
 {
 	int pdsum = 0;
 	int a = 0;
-	int b = b;
+	int b = 0;
 	int sum = 0;
-
-
-	for(int i = 2; i < 10000; ++i)
+	int s1 = 0;
+	int s2 = 0;
+	
+	for(int i = 2; i < 20; ++i)
 	{
 		a = i;
-		propDiv(i, a);
-		propDiv(a, b);
-		if( a != b )
-
+		std::cout << a << ' ';
+		propDiv(a, s1);
+		std::cout << std::endl;
+		b = s1;
+		std::cout << b << ' ';
+		propDiv(b, s2);
+		std::cout << std::endl;
+		if( a == s2)
+		{
+			sum += a;
+			sum += b;
+		}
 	}
-
+	std::cout << sum << std::endl;
 	return 0;
 }
